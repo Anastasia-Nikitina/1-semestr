c23eb5b1c2e4305ade685e227f7022e59d59958b
Anastasia-Nikitina <nasty.nickitina2011@yandex.ru>
2020-10-15 13:07:56 +0300

    fixed

--- src/homework_2/FunctionsHw3.fs
+++ src/homework_2/FunctionsHw3.fs
@@ -3,7 +3,7 @@ open System
 
   
 let rec fib1 n =
-    if n=0 || n=1
+    if n = 0 || n = 1
     then n
     elif n > 1
     then fib1(n-1) + fib1(n-2)
@@ -40,20 +40,30 @@ let fib3 n =
 
 
 let multMatrix (a: int [,]) (b: int [,]) =
-    let res = Array2D. zeroCreate (a.GetLength 0) (b.GetLength 1)
-    for i = 0 to (a.GetLength 0 - 1) do
-      for j = 0 to (b.GetLength 1 - 1) do
-       for l = 0 to (a.GetLength 1 - 1) do
-          res.[i, j] <- res.[i, j] + a.[i, l]*b.[l, j]
-    res
-    
-let identityMatrix = array2D [|[|1; 0|]; [|0; 1|]|]
+    if a.GetLength 0 = b.GetLength 1
+    then
+      let res = Array2D.zeroCreate (a.GetLength 0) (b.GetLength 1)
+      for i = 0 to (a.GetLength 0 - 1) do
+        for j = 0 to (b.GetLength 1 - 1) do
+          for l = 0 to (a.GetLength 1 - 1) do
+            res.[i, j] <- res.[i, j] + a.[i, l]*b.[l, j]
+      res
+    else failwith "Size of matrices is not correct"
 
-let rec powMatrix m n =
+
+let identityMatrix n =
+    let matrix = Array2D.zeroCreate n n
+    for i = 0 to n-1 do
+        for j = 0 to n-1 do
+            if i = j
+            then matrix.[i, j] <- 1
+    matrix
+
+let rec powMatrix (m: int [,]) n =
     if n = 0
-    then identityMatrix
+    then identityMatrix (m.GetLength 0)
     else multMatrix m (powMatrix m (n-1))
-
+    
 let fib4 n =
     if n >= 0
     then
@@ -67,9 +77,11 @@ let fib5 n =
     then
         let arr = array2D [|[|0; 1|]; [|1; 1|]|]
         let rec _go arr n =
-          if n = 0 then identityMatrix
+          if n = 0 then identityMatrix 2
           elif n % 2 = 0
-          then multMatrix (_go arr (n/2))(_go arr (n/2))
+          then
+              let k = _go arr (n/2)
+              multMatrix k k
           else multMatrix (_go arr (n-1)) arr
         let res = _go arr n
         res.[0, 1]
@@ -77,18 +89,14 @@ let fib5 n =
 
 
 let fib6 n =
-    let arr: int array = Array.zeroCreate (n + 1)
-    if n = 0 || n = 1
+    let arr = Array.zeroCreate (n + 1)
+    if  n > 0
     then
-        for i = 0 to n do
-            arr.[n] <- n
-    elif n>1
-    then
-         arr.[0] <- 0
-         arr.[1] <- 1
-         for i = 2 to n do
-           arr.[i] <- arr.[i-1] + arr.[i-2]
-    else failwith "n must be non-negative"
+        arr.[1] <- 1
+        for i = 2 to n do
+              arr.[i] <- arr.[i-1] + arr.[i-2]   
+    elif n < 0
+    then failwith "n must be non-negative"
     arr
 
 
