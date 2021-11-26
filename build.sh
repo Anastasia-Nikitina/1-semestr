0ce81675ff8cbeb542f4c8101c5a326c9805a812
Anastasia-Nikitina <nasty.nickitina2011@yandex.ru>
2021-11-26 22:36:06 +0300

    fixed script

--- build.sh
+++ build.sh
@@ -1,9 +1,4 @@
-#!/usr/bin/env bash
+#! /bin/bash
 
-set -eu
-set -o pipefail
-
-echo "Restoring dotnet tools..."
 dotnet tool restore
-
-PAKET_SKIP_RESTORE_TARGETS=true FAKE_DETAILED_ERRORS=true dotnet fake build -t "$@"
+dotnet fake build
